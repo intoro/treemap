0bf6348bbdf1b4b714355c82b85160f20098f983
intoro <tmcs.brian@gmail.com>
2017-03-29 23:37:33 -0700

    input for type

--- treemap.py
+++ treemap.py
@@ -21,22 +21,6 @@ def home(error_message=None):
     trees = json.dumps(trees)
     return render_template("home.html", trees=trees, categories=categories, error_message=error_message)
 
-# @app.route("/add", methods=["POST"])
-# def add():
-#   try:
-#     data = request.form.get("userinput")
-#     DB.add_input(data)
-#   except Exception as e:
-#     print e
-#   return home()
-#
-# @app.route("/clear")
-# def clear():
-#   try:
-#     DB.clear_all()
-#   except Exception as e:
-#     print e
-#   return home()
 
 @app.route("/submittree", methods=['POST'])
 def submittree():
